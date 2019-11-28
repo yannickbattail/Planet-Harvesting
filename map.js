fe6fc5f46f05f4a393c94aeb9e3812586693780f
Yannick Battail <yannick.battail@raisepartner.com>
2019-11-28 20:04:08 +0100

    separate input config and events

--- map.js
+++ map.js
@@ -28,17 +28,18 @@ function recolorPolygonesFromHighs(polygonList, color) {
     );
 }
 
-function addhill(polygonList, start, high, radius, sharpness) {
+function addhill(polygonList, start, config) {
   var queue = [];
+  var high = config.high;
   polygonList[start].high += high;
   polygonList[start].used = 1;
   queue.push(start);
   for (let i = 0; i < queue.length && high > 0.01; i++) {
-    high = high * radius;
+    high = high * config.radius;
     polygonList[queue[i]].neighbors
       .filter(e => !polygonList[e].used)
       .forEach(e => {
-        polygonList[e].high = computeNewHeight(polygonList[e].high, high, sharpness);
+        polygonList[e].high = computeNewHeight(polygonList[e].high, high, config.sharpness);
         polygonList[e].used = true;
         queue.push(e);
       });
@@ -58,59 +59,73 @@ function computeNewHeight(previousHeight, height, sharpness) {
   return newHeight;
 }
 
+function createHill(config, point, diagram, mapCells, polygons, color) {
+  var nearest = diagram.find(point[0], point[1]).index;
+  mapCells.append("circle")
+    .attr("r", 3)
+    .attr("cx", point[0])
+    .attr("cy", point[1])
+    .attr("fill", color(1 - config.high))
+    .attr("class", "circle");
+  addhill(polygons, nearest, config);
+  recolorPolygonesFromHighs(polygons, color);
+}
+
+function drawMouseCircle(config, point, diagram, svg, polygons) {
+  var nearest = diagram.find(point[0], point[1]).index;
+  var radius = config.radius * 50;
+  $("#cell").text(nearest);
+  $("#high").text((polygons[nearest].high).toFixed(2));
+  svg.select(".radius").remove();
+  svg.append("circle")
+    .attr("r", radius)
+    .attr("cx", point[0])
+    .attr("cy", point[1])
+    .attr("class", "radius");
+}
+
+function getConfig() {
+  return {
+    size: parseInt(sizeInput.valueAsNumber),
+    high: parseFloat(highInput.valueAsNumber),
+    radius: parseFloat(radiusInput.valueAsNumber),
+    sharpness: parseFloat(sharpnessInput.valueAsNumber)
+  };
+}
+
 function generate() {
+  var config = getConfig();
   d3.select(".mapCells").remove();
   var svg = d3.select("svg");
-  var mapCells = svg.append("g").attr("class", "mapCells")
-        .on("touchmove mousemove", drawMouseCircle)
-        .on("click", createHill);
-  var width = parseInt(svg.attr("width"));
-  var height = parseInt(svg.attr("height"));
-  var sites = d3.range(sizeInput.valueAsNumber).map(
-      () => [Math.random() * width, Math.random() * height]
+  var mapCells = svg.append("g")
+                    .attr("class", "mapCells")
+                    .on("touchmove mousemove", eventDdrawMouseCircle)
+                    .on("click", eventCreateHill);
+  var imgWidth = parseInt(svg.attr("width"));
+  var imgHeight = parseInt(svg.attr("height"));
+  var sites = d3.range(config.size).map(
+      () => [Math.random() * imgWidth, Math.random() * imgHeight]
     );
-  var voronoi = d3.voronoi().extent([[0, 0],[width, height]]);
+  var voronoi = d3.voronoi().extent([[0, 0],[imgWidth, imgHeight]]);
   sites = voronoi(sites).polygons().map(d3.polygonCentroid);
   var diagram = voronoi(sites);
   var polygons = diagram.polygons();
   var color = d3.scaleSequential(d3.interpolateSpectral);
 
-  // change options to defaults for hills
-  highInput.value = 0.5;
-  highOutput.value = 0.5;
-  radiusInput.value = 0.99;
-  radiusOutput.value = 0.99;
-
+  function eventCreateHill() {
+    var config = getConfig();
+    var point = d3.mouse(this);
+    createHill(config, point, diagram, mapCells, polygons, color);
+  }
+  
+  function eventDdrawMouseCircle() {
+    var config = getConfig();
+    var point = d3.mouse(this);
+    drawMouseCircle(config, point, diagram, svg, polygons);
+  }
+  
   detectNeighbors(polygons, diagram);
   polygonAppendPath(polygons, mapCells, color);
-
-
-  function createHill(e) {
-    var point = d3.mouse(this);
-    var nearest = diagram.find(point[0], point[1]).index;
-    mapCells.append("circle")
-      .attr("r", 3)
-      .attr("cx", point[0])
-      .attr("cy", point[1])
-      .attr("fill", color(1 - highInput.valueAsNumber))
-      .attr("class", "circle");
-    addhill(polygons, nearest, highInput.valueAsNumber, radiusInput.valueAsNumber, sharpnessInput.valueAsNumber);
-    recolorPolygonesFromHighs(polygons, color);
-  }
-
-  function drawMouseCircle() {
-    var point = d3.mouse(this);
-    var nearest = diagram.find(point[0], point[1]).index;
-    var radius = radiusInput.valueAsNumber * 50;
-    $("#cell").text(nearest);
-    $("#high").text((polygons[nearest].high).toFixed(2));
-    svg.select(".radius").remove();
-    svg.append("circle")
-      .attr("r", radius)
-      .attr("cx", point[0])
-      .attr("cy", point[1])
-      .attr("class", "radius");
-  }
 }
 
 generate();
