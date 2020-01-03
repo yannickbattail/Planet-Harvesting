c0963ba36f556081f874786bcd4ff3f2a5eb524b
Yannick Battail <yannick.battail@raisepartner.com>
2020-01-03 17:29:32 +0100

    set global maps points and elevation

--- map2.js
+++ map2.js
@@ -1,12 +1,18 @@
 "use strict";
 /*globals $:false */
 /*globals d3:false */
+/*globals L:false */
+/*globals window:false */
 
 function getColorFormHeight(high) {
   if (high == 0) return "#000080";
   var color = d3.scaleSequential(d3.interpolateSpectral);
   //return color(Math.random());
   return color(1-high);
+}
+
+function pow2(x) {
+  return Math.pow(2, x);
 }
 
 function detectNeighbors(polygonList, diagramme) {
@@ -69,7 +75,7 @@ function computeNewHeight(previousHeight, height, sharpness) {
   return newHeight;
 }
 
-function createHill(config, point, diagram, polygons, mapCells, svg) {
+function createHill(config, point, diagram, polygons, mapCells) {
   var nearest = diagram.find(point[0], point[1]).index;
   mapCells.append("circle")
     .attr("r", 3)
@@ -78,40 +84,104 @@ function createHill(config, point, diagram, polygons, mapCells, svg) {
     .attr("fill", getColorFormHeight(config.high))
     .attr("class", "circle");
   addhill(polygons, nearest, config);
-  recolorPolygonesFromHighs(polygons, svg);
 }
 
 function getConfig() {
   return {
-    size: 1000,
+    imgSize: 256,
+    size: 5000,
     high: 0.5,
     radius: 0.992,
     sharpness: 0.2
   };
 }
 
-function generate() {
-  var imgWidth = 256;
-  var imgHeight = 256;
+function generate(coords) {
   var config = getConfig();
   var svg = d3.create("svg");
-  svg.attr("width", imgWidth);
-  svg.attr("height", imgHeight);
+  svg.attr("width", config.imgSize);
+  svg.attr("height", config.imgSize);
   var mapCells = svg.append("g").attr("class", "mapCells");
-  var sites = d3.range(config.size).map(
-      () => [Math.random() * imgWidth, Math.random() * imgHeight]
-    );
-  var voronoi = d3.voronoi().extent([[0, 0],[imgWidth, imgHeight]]);
-  sites = voronoi(sites).polygons().map(d3.polygonCentroid);
+  var voronoi = d3.voronoi().extent([[0, 0],[config.imgSize, config.imgSize]]);
+  var sites = cutPolygList(coords, polygCenters, config.imgSize);
   var diagram = voronoi(sites);
   var polygons = diagram.polygons();
 
   detectNeighbors(polygons, diagram);
   polygonAppendPath(polygons, mapCells);
-  createHill(getConfig(), [imgWidth/2, imgHeight/2], diagram, polygons, mapCells, svg);
+  initHeight(diagram, polygons, sites);
+  //createHill(getConfig(), [config.imgSize/2, config.imgSize/2], diagram, polygons, mapCells);
+  recolorPolygonesFromHighs(polygons, svg);
   return svg.node();
 }
 
+function initHeight(diagram, polygons, sites) {
+  sites.forEach(
+    p => {
+      var idx = diagram.find(p[0], p[1]).index;
+      polygons[idx].high = p[2]
+    }
+  );
+}
+
+function isInBoundaries(point, boundaries) {
+  return point[0] > boundaries.min.x
+      && point[0] <= boundaries.max.x
+      && point[1] > boundaries.min.y
+      && point[1] <= boundaries.max.y;
+}
+
+function zoom(point, origin, coef) {
+  return [
+    (point[0] - origin.x) * coef,
+    (point[1] - origin.y) * coef,
+    point[2]
+  ];
+}
+
+function cutPolygList(coords, centers, size) {
+  if (coords.z == 0) return centers;
+  var coef = pow2(coords.z);
+  var dim = size / coef;
+  var boundaries = {
+    min: {
+      x: dim * coords.x,
+      y: dim * coords.y,
+    },
+    max: {
+      x: dim * (coords.x + 1),
+      y: dim * (coords.y + 1),
+    },
+  };
+  return centers.filter(
+    p => isInBoundaries(p, boundaries)
+  ).map(
+    p => zoom(p, boundaries.min, coef)
+  );
+}
+
+function init() {
+  var config = getConfig();
+  var sites = d3.range(config.size).map(
+    () => [Math.random() * config.imgSize, Math.random() * config.imgSize]
+  );
+  var voronoi = d3.voronoi().extent([[0, 0],[config.imgSize, config.imgSize]]);
+  sites = voronoi(sites).polygons().map(d3.polygonCentroid);
+  volcano(sites, [config.imgSize/2, config.imgSize/2]);
+  return sites;
+}
+
+function distance(a, b) {
+  return Math.sqrt( Math.pow((a[0]-b[0]), 2) + Math.pow((a[1]-b[1]), 2) );
+}
+
+function volcano(sites, middle) {
+  sites.forEach(
+    p => p[2] = 1 / Math.sqrt(distance(p, middle))
+  );
+}
+
+var polygCenters = init();
 var map = L.map('map', {
   center: [0, 0],
   zoom: 0
@@ -119,8 +189,8 @@ var map = L.map('map', {
 
 L.GridLayer.DebugCoords = L.GridLayer.extend({
   createTile: function (coords, done) {
-    var tile = generate();
-    setTimeout(function () {
+    var tile = generate(coords);
+    window.setTimeout(function () {
       // Syntax is 'done(error, tile)'
       done(null, tile);
     }, 50);
